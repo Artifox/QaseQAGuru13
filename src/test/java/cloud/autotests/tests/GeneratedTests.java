bb3d86371d37b3764c2b9c2e260b227b8b223f21
Artsiom Koptsiukh <artsiom.koptsiukh@customertimes.com>
2022-10-30 18:58:44 +0400

    Added cases

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -1,10 +1,12 @@
 package cloud.autotests.tests;
 
-import cloud.autotests.helpers.DriverUtils;
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.title;
 import static io.qameta.allure.Allure.step;
@@ -13,11 +15,80 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GeneratedTests extends TestBase {
     @Test
-    @Description("Soon to be implemented by you (or QA.GURU engineers)")
-    @DisplayName("QASETestProject13")
-    void generatedTest() {
-        step("Open \"qase.io\"", () -> {
-            step("// todo: just add selenium action");
+    @Description("Test verify that page title is displayed on main page")
+    @DisplayName("Page title should have header text")
+    void verifyPageTitleTest() {
+        step("Open url 'https://qase.io/'", () ->
+                open("https://qase.io/"));
+
+        step("Page title should have text 'Test case management software'", () -> {
+            String expectedTitle = "Test case management software";
+            String actualTitle = title();
+            assertThat(actualTitle).contains(expectedTitle);
+        });
+    }
+
+    @Test
+    @DisplayName("Unauthorized user should have the ability to open login page")
+    @Description("Unauthorized user can navigate to login page from main page")
+    void verifyAbilityToOpenLoginPageForUnauthorizedUserTest() {
+        step("Open url 'https://qase.io/'", () ->
+                open("https://qase.io/"));
+
+        step("Click on Login button", () -> {
+            $("a#signin").click();
+        });
+
+        step("Check that login page is opened", () -> {
+            $("a.logo").shouldBe(Condition.visible);
+        });
+    }
+
+    @Test
+    @DisplayName("Unauthorized user should have the ability to open price page")
+    @Description("Unauthorized user can navigate to price page from main page")
+    void verifyAbilityToOpenPricePageForUnauthorizedUserTest() {
+        step("Open url 'https://qase.io/'", () ->
+                open("https://qase.io/"));
+
+        step("Click on Login button", () -> {
+            $(byText("Pricing")).click();
+        });
+
+        step("Check that price page is opened", () -> {
+            $(".flexible-plans").shouldHave(Condition.text("Flexible plans for any team"));
+        });
+    }
+
+    @Test
+    @DisplayName("Unauthorized user should have the ability to open blog page")
+    @Description("Unauthorized user can navigate to blog page from main page")
+    void verifyAbilityToOpenBlogPageForUnauthorizedUserTest() {
+        step("Open url 'https://qase.io/'", () ->
+                open("https://qase.io/"));
+
+        step("Click on Login button", () -> {
+            $(byText("Blog")).click();
+        });
+
+        step("Check that blog page is opened", () -> {
+            $(".site-logo").shouldBe(Condition.visible);
+        });
+    }
+
+    @Test
+    @DisplayName("Unauthorized user should have the ability to open documentation page")
+    @Description("Unauthorized user can navigate to documentation page from main page")
+    void verifyAbilityToOpenDocumentationPageForUnauthorizedUserTest() {
+        step("Open url 'https://qase.io/'", () ->
+                open("https://qase.io/"));
+
+        step("Click on Login button", () -> {
+            $(byText("Developers")).click();
+        });
+
+        step("Check that blog page is opened", () -> {
+            $(byText("Documentation")).shouldBe(Condition.visible);
         });
     }
 }
